9b7c690681ffef151847322af4e00f45f6bbe0d3
Yang Jianming <yajimicx330@gmail.com>
2026-02-03 20:39:46 +0800

    实现轮播图效果渲染

--- lib/components/Home/HmSlider.dart
+++ lib/components/Home/HmSlider.dart
@@ -1,15 +1,37 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:hm_shop/viewmodels/home.dart';
 
 class HmSlider extends StatefulWidget {
-  const HmSlider({super.key});
+  final List<BannerItem> bannerList;
+  const HmSlider({super.key, required this.bannerList});
 
   @override
   State<HmSlider> createState() => _HmSliderState();
 }
 
 class _HmSliderState extends State<HmSlider> {
+  Widget _getSlider(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    return CarouselSlider(
+      items: List.generate(widget.bannerList.length, (int index) {
+        return Image.network(
+          widget.bannerList[index].imgUrl,
+          fit: BoxFit.cover,
+          width: screenWidth,
+        );
+      }),
+      options: CarouselOptions(
+        height: 300,
+        viewportFraction: 1,
+        autoPlay: true,
+        autoPlayInterval: Duration(seconds: 1),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text("轮播图"),);
+    return Stack(children: [_getSlider(context)]);
   }
 }
--- lib/pages/Home/index.dart
+++ lib/pages/Home/index.dart
@@ -4,6 +4,7 @@ import 'package:hm_shop/components/Home/HmHot.dart';
 import 'package:hm_shop/components/Home/HmMoreList.dart';
 import 'package:hm_shop/components/Home/HmSlider.dart';
 import 'package:hm_shop/components/Home/HmSuggestion.dart';
+import 'package:hm_shop/viewmodels/home.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -13,9 +14,23 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  final List<BannerItem> _bannerList = [
+    BannerItem(
+      id: "1",
+      imgUrl: "https://yjy-teach-oss.oss-cn-beijing.aliyuncs.com/meituan/1.jpg",
+    ),
+    BannerItem(
+      id: "2",
+      imgUrl: "https://yjy-teach-oss.oss-cn-beijing.aliyuncs.com/meituan/2.png",
+    ),
+    BannerItem(
+      id: "3",
+      imgUrl: "https://yjy-teach-oss.oss-cn-beijing.aliyuncs.com/meituan/3.jpg",
+    ),
+  ];
   List<Widget> _getScrollChildren() {
     return [
-      SliverToBoxAdapter(child: HmSlider()),
+      SliverToBoxAdapter(child: HmSlider(bannerList: _bannerList)),
       SliverToBoxAdapter(child: HmCategory()),
       SliverToBoxAdapter(child: HmSuggestion()),
       SliverToBoxAdapter(
