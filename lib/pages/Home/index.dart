5430879d9cf26ead9175b98659ee777bc6287726
Yang Jianming <yajimicx330@gmail.com>
2026-02-03 15:50:19 +0800

    主页tab栏实现

---
+++ lib/pages/Home/index.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("首页页面"),
+    );
+  }
+}
--- lib/pages/Main/index.dart
+++ lib/pages/Main/index.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:hm_shop/pages/Home/index.dart';
+import 'package:hm_shop/pages/Category/index.dart';
+import 'package:hm_shop/pages/Cart/index.dart';
+import 'package:hm_shop/pages/Mine/index.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -8,11 +12,63 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  final List<Map<String, String>> _tabList = [
+    {
+      "icon": "lib/assets/ic_public_home_normal.png",
+      "active_icon": "lib/assets/ic_public_home_active.png",
+      "text": "主页",
+    },
+    {
+      "icon": "lib/assets/ic_public_pro_normal.png",
+      "active_icon": "lib/assets/ic_public_pro_active.png",
+      "text": "分类",
+    },
+    {
+      "icon": "lib/assets/ic_public_cart_normal.png",
+      "active_icon": "lib/assets/ic_public_cart_active.png",
+      "text": "购物车",
+    },
+    {
+      "icon": "lib/assets/ic_public_my_normal.png",
+      "active_icon": "lib/assets/ic_public_my_active.png",
+      "text": "我的",
+    },
+  ];
+  int _currentIndex = 0;
+
+  List<BottomNavigationBarItem> _getTabBarWidget() {
+    return List.generate(_tabList.length, (int index) {
+      return BottomNavigationBarItem(
+        icon: Image.asset(_tabList[index]["icon"]!, width: 24, height: 24),
+        activeIcon: Image.asset(
+          _tabList[index]["active_icon"]!,
+          width: 24,
+          height: 24,
+        ),
+        label: _tabList[index]["text"],
+      );
+    });
+  }
+
+  List<Widget> _getChildren() {
+    return [HomeView(), CategoryView(), CartView(), MineView()];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Main Page")),
-      body: Center(child: Text("Welcome to the Main Page")),
+      body: SafeArea(
+        child: IndexedStack(index: _currentIndex, children: _getChildren()),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (int index) {
+          _currentIndex = index;
+          setState(() {});
+        },
+        currentIndex: _currentIndex,
+        items: _getTabBarWidget(),
+      ),
     );
   }
 }
